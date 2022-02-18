7b2086c5436a95c4dd66750925f65bf998eecad8
momolela <1083910359@qq.com>
2022-02-19 01:46:57 +0800

    20220219

---
+++ 01-start/build/start.js
@@ -0,0 +1,8 @@
+"use strict";
+let num1 = 6;
+let num2 = 6;
+function add(n1, n2) {
+    return n1 + n2;
+}
+let total = add(num1, num2);
+console.log(total);
---
+++ 01-start/src/start.ts
@@ -0,0 +1,9 @@
+let num1:number = 6;
+let num2:number = 6;
+
+function add(n1:number, n2:number) :number {
+    return n1 + n2;
+}
+
+let total:number = add(num1, num2);
+console.log(total);
